665c1ca53e4412e7744c15040b5be61e72ac8ced
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-10-23 17:38:46 +0300

    coding is starting

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,30 @@
 /* eslint-disable consistent-return */
 //получаем нужные эл-ты
 const button = document.querySelector('.container');
+document.addEventListener('DOMContentLoaded', getPage);
+let xhr = new XMLHttpRequest();
+let data;
+
+function getPage() {
+  console.log('ok');
+  xhr.open('GET', 'http://localhost:7070/');
+  xhr.send();
+  xhr.onloadend = function() {
+    data = JSON.parse(this.responseText);
+    console.log(data);
+    data.forEach((element) => {
+      taskList.innerHTML += `<div><div>${element.id}</div><div>${element.description}</div></div>`
+    });
+  };
+  const taskList = document.createElement('div');
+  taskList.setAttribute('class', 'task-list');
+  taskList.setAttribute('data-id', 'task-list');
+  button.insertAdjacentElement('afterend', taskList);
+}
+
 
 function fn() {
   console.log('ok');
-  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://localhost:7070/');
   xhr.send();
   xhr.onloadend = function() {
